153bcac20995636316ac357d36b88c33acf6cfc0
Binith001 <95204552+Binith001@users.noreply.github.com>
2022-05-30 06:34:32 +0000

    4a

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,1 @@
-# Functions
 
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
